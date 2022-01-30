ca589179438116e8b1c2813f5c145d465b8f7844
Nicolás Mafla <nicolas.mafla@altscore.ai>
2022-01-29 19:08:11 -0500

    model

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+import random
 from models.rw_generator import RwGenerator
 
+random.seed(1)
+
+names = ["Enid", "Nixie", "Fawn", "Clarion", "Juno", "Tiana", "Korra", "Aine", "Ondine", "Ella", "Circe", "Jadis",
+         "Endora", "Sybil", "Piper", "Nimue", "Tara", "Clio", "Iris", "Daria", "Kouzlo", "Yadu", "Sen", "Caerus",
+         "Blythe", "Buyu", "Taika", "Aether", "Pontus", "Zephyr"]
+
+random.shuffle(names)
+
 if __name__ == "__main__":
-  r = RwGenerator()
+    for i in range(1, 31):
+        size = 800
+        bg = (0, 0, 0)
+        r = RwGenerator(size, bg, 1)
+        r.draw_random_walk(steps=500, walks=3)
+        r.save_image(f"rw_dream_{names[i - 1].lower()}")
+        print(f"Random walk dream {names[i - 1]} created")
--- models/rw_generator.py
+++ models/rw_generator.py
@@ -1,6 +1,46 @@
-from PIL import Image
+import random
+import numpy as np
+from PIL import Image, ImageDraw
+
 
 class RwGenerator:
-    def __init__(self):
-        self.size_px = None
-        self.bg_color = None
+    def __init__(self, size, bg_color, seed):
+        self.size = size
+        self.image = Image.new(mode="RGB", size=(size, size), color=bg_color)
+
+    def draw_random_walk(self, steps, walks):
+        draw = ImageDraw.Draw(self.image)
+
+        step_shape = (steps, 2)
+
+        repeat = walks
+
+        while repeat > 0:
+            center = np.array([np.random.normal(self.size / 2, 25, 2)])
+            steps = np.random.choice(a=[-15, -10, 0, 10, 15], size=step_shape)
+            path = np.concatenate([center, steps]).cumsum(0)
+
+            min_x, min_y = path.min(axis=0)
+            max_x, max_y = path.max(axis=0)
+            delta_x = (min_x + max_x - self.size) / 2
+            delta_y = (min_y + max_y - self.size) / 2
+            path[:, 0] = path[:, 0] - delta_x
+            path[:, 1] = path[:, 1] - delta_y
+
+            thickness = range(1, 6)
+            line_color = (np.random.randint(255), np.random.randint(255), np.random.randint(255))
+
+            for i in range(len(path) - 1):
+                l1 = line_color[0] + (np.random.randint(36) * random.choice([1, -1]))
+                l2 = line_color[1] + (np.random.randint(36) * random.choice([1, -1]))
+                l3 = line_color[2] + (np.random.randint(36) * random.choice([1, -1]))
+                new_line_color = (l1, l2, l3)
+                initial = tuple(path[i])
+                final = tuple(path[i + 1])
+                line = (initial, final)
+                draw.line(line, fill=new_line_color, width=random.choice(thickness))
+
+            repeat -= 1
+
+    def save_image(self, name):
+        self.image.save(f"./outputs/{name}.png")
